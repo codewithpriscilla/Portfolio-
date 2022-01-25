a2e0387a4445346bf3e8e29cf8fa4cb2c388af32
Priscilla Martinez <priscillamartinez@PriscillasMBP2.attlocal.net>
2022-01-25 14:45:05 -0800

    Nav Bar

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+if (typeof $ == 'undefined'){
+    console.log('oops! I still have to link my jQuery properly!');
+} else {
+    console.log('I did it! I linked jQuery and this js file properly!')
+};
 
+ 
+// ...............................................
+//      N A V - B A R
+
+$(".icon").click(function() {
+    let x = document.getElementById("myLinks");
+
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+})
+
+$(".logo").click(function() {
+    let x = document.getElementById("myLinks");
+    
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+})
